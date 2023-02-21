bcb1d8e7a4e14be39f42333767d87da52bc05785
Ivan Gagis <igagis@gmail.com>
2023-02-21 10:16:55 +0200

    first potential memory leak

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
+#include <memory>
+
+void potential_memory_leak_1(){
+    auto f = [obj = std::make_shared<std::string>("Hello world!")](){
+        std::cout << *obj << std::endl;
+    };
+
+    f();
+}
+
 int main(int argc, const char** argv){
 
-    
+    potential_memory_leak_1();
 
     return 0;
 }
